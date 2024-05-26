54db2dac0078265cb6d6ed86a7c39c8970e11565
code-cp <moshan.nus@gmail.com>
2024-05-26 08:53:57 +0800

    change to color

--- src/infer.rs
+++ src/infer.rs
@@ -1,7 +1,4 @@
-use burn::backend::wgpu::WgpuDevice;
 use burn::{
-    config::Config,
-    data::dataloader::batcher::Batcher,
     module::Module,
     record::{
         BinFileRecorder, NoStdTrainingRecorder, CompactRecorder, FullPrecisionSettings, NamedMpkFileRecorder, Recorder,
@@ -9,7 +6,6 @@ use burn::{
     tensor::backend::AutodiffBackend,
     tensor::{activation, backend::Backend, Bool, Device, ElementConversion, Int, Tensor},
 };
-use burn_tensor::Data;
 use image::ImageBuffer;
 use ndarray::{s, Array, Array1, Array2, Array3, Axis};
 use std::env;
@@ -23,8 +19,8 @@ pub fn load_model<B: Backend>(device: &B::Device) -> Model<B> {
     // Load model in full precision from MessagePack file
     let model_path = model_dir.join("model.bin");
 
-    let recorder: NamedMpkFileRecorder<burn::record::HalfPrecisionSettings> = CompactRecorder::new();
-    // let recorder = NoStdTrainingRecorder::new();
+    // let recorder: NamedMpkFileRecorder<burn::record::HalfPrecisionSettings> = CompactRecorder::new();
+    let recorder = NoStdTrainingRecorder::new();
 
     let model = Model::new(device);
     let model = model
@@ -45,16 +41,18 @@ pub fn infer<B: Backend>(device: &B::Device) {
     let image: Tensor<B, 3> = image.squeeze(0);
     let image = image.clone() * 255.;
 
-    let arr = Array2::from_shape_vec(
-        (image.dims()[1] as usize, image.dims()[2] as usize),
+    let arr = Array3::from_shape_vec(
+        (image.dims()[0] as usize, image.dims()[1] as usize, image.dims()[2] as usize),
         image.clone().into_data().convert::<f32>().value,
     )
     .unwrap();
 
     let image_buffer =
         ImageBuffer::from_fn(image.dims()[1] as u32, image.dims()[2] as u32, |x, y| {
-            let pixel = arr[[y as usize, x as usize]];
-            image::Luma([pixel as u8])
+            let r = arr[[0, y as usize, x as usize]];
+            let g = arr[[1, y as usize, x as usize]];
+            let b = arr[[2, y as usize, x as usize]];
+            image::Rgb([r, g, b])
         });
 
     image_buffer.save(format!("./images/result.png")).unwrap();
